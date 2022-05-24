41854c484fcce16568528b8e939e970ab650983c
Qhewzi <qhewzi.heng.2020@sis.smu.edu.sg>
2022-05-25 00:24:29 +0800

    Changed the layout of the buttons
    Changed the layout of the buttons

--- hangman-game/src/App.js
+++ hangman-game/src/App.js
@@ -1,9 +1,7 @@
 import GameLogo from './GameLogo.svg';
-import SingleplayerButton from './SingleplayerButton.svg';
-import MultiplayerButton from './MultiplayerButton.svg';
-import PlayButton from './PlayButton.svg';
 import HelpButton from './HelpButton.svg';
 import './App.css';
+import PlayerButtons from './PlayerButtons'
 
 function App() {
   return (
@@ -12,7 +10,11 @@ function App() {
         <text className="Title">
           Hangman
         </text>
-        <img src={GameLogo} className="App-logo" alt="logo"/>
+        <div className="game-container">
+          <img src={GameLogo} className="App-logo" alt="logo"/>
+          <PlayerButtons />
+        </div>
+        <img src={HelpButton} className="HelpButton" alt="help=button"/>
         {/* <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,12 +23,7 @@ function App() {
         >
           Learn React
         </a> */}
-        <div className="PlayerButtons">
-          <img src={SingleplayerButton} className="SingleplayerButton" alt="singleplayer-button"/>
-          <img src={MultiplayerButton} className="MultiplayerButton" alt="multiplayer-button"/>
-        </div>
-        <img src={PlayButton} className="PlayButton" alt="play-button"/>
-        <img src={HelpButton} className="HelpButton" alt="help=button"/>
+
       </header>
     </div>
   );
